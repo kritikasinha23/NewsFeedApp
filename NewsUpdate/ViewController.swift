7d0da614cd02699b0539124bce930b4d3a20b1f6
kritikasinha23 <100464424+kritikasinha23@users.noreply.github.com>
2022-05-20 20:12:16 +0530

    Initial Project Setup

---
+++ NewsUpdate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsUpdate
+//
+//  Created by kritika sinha on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
